0d16d65de8dc9160e3eb3d3b25128aa15676bb81
Silvave <todor.vasilev@icb.bg>
2018-07-06 16:13:04 +0300

    Added and configured SignalR Hub for streeming data from console to client app

---
+++ UpdateQ.Hub/Hubs/SensorHub.cs
@@ -0,0 +1,17 @@
+﻿namespace UpdateQ.Hub.Hubs
+{
+    using Microsoft.AspNetCore.SignalR;
+    using System.Threading.Tasks;
+    using UpdateQ.Simulator.Model;
+
+    public class SensorHub : Hub
+    {
+        public async Task SendSensorData(SensorDto sensorDto)
+        {
+            await Clients.All.SendAsync("ReceiveSensorData", sensorDto);
+
+            // Send to a single user w/ multiple connections 
+            //await Clients.User(userId).SendAsync("ReceiveSesorData", sensorDto);
+        }
+    }
+}
